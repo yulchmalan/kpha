5efa98c740edf803a6edcfde5aadd13a80e304a7
yulch <159718838+yulchmalan@users.noreply.github.com>
2024-12-02 16:08:59 +0200

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+      dynamicBullets: true,
+      dynamicMainBullets: 1,
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+      dragSize: 20,
+    },
+
+    slidesPerView: 1.9,
+    spaceBetween: 30,
+    breakpoints: {
+        769: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 5,
+        },
+      },
+  });
